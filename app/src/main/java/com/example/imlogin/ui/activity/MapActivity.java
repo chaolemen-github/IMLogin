836aa3c96afe10a9597f844abf1571ee2a1bae1d
chaolemen-github <2863346378@qq.com>
2020-06-11 21:56:52 +0800

    地图

--- app/src/main/java/com/example/imlogin/ui/activity/MapActivity.java
+++ app/src/main/java/com/example/imlogin/ui/activity/MapActivity.java
@@ -2,24 +2,75 @@ package com.example.imlogin.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 import com.example.imlogin.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements View.OnClickListener {
 
     private MapView mMapView;
+    private Button mLocationBtn;
+    private BaiduMap baiduMap;
+    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         initView();
+        initLocation();
+    }
+
+    private void initLocation() {
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+        //注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+        //开启地图定位图层
+        mLocationClient.start();
+    }
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            baiduMap.setMyLocationData(locData);
+        }
     }
 
     private void initView() {
         mMapView = (MapView) findViewById(R.id.mapView);
-
+        mLocationBtn = (Button) findViewById(R.id.btn_location);
+        mLocationBtn.setOnClickListener(this);
+        baiduMap = mMapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
     }
 
     @Override
@@ -37,6 +88,20 @@ public class MapActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mLocationClient.stop();
+        baiduMap.setMyLocationEnabled(false);
         mMapView.onDestroy();
+        mMapView=null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_location:
+                // TODO 20/06/11
+                break;
+            default:
+                break;
+        }
     }
 }
